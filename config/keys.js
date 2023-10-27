c093a02867eda2a785547abeb5c57e813e83f812
lordmarasmus <145048142+lordmarasmus@users.noreply.github.com>
2023-10-27 09:47:38 -0700

    M

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://evereddyer914:Jefered50@cluster0.l5tlr1m.mongodb.net/?retryWrites=true&w=majority';
+dbPassword = 'mongodb+srv://evereddyer914:Jefered50@cluster0.l5tlr1m.mongodb.net/SPACE_COAST?retryWrites=true&w=majority'
 
 module.exports = {
     mongoURI: dbPassword
