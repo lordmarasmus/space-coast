18daa1c41285489459254e540a6656b513c18aff
stefan <38575516+stefanlester@users.noreply.github.com>
2023-10-28 09:04:19 -0700

    m

--- app1.js
+++ app1.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'sccu-app/alerts/public')));
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
