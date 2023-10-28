129e4764530195b5dad4f417fac127aff1e36236
stefan <38575516+stefanlester@users.noreply.github.com>
2023-10-28 08:23:29 -0700

    m

--- app1.js
+++ app1.js
@@ -66,7 +66,7 @@ app.use(function(req, res, next) {
 
 // Routes
 app.use('/', require('./routes/index.js'));
-app.use('/users', require('./routes/users.js'));
+app.use('/sccu-app/alerts', require('./routes/users.js'));
 
 const PORT = process.env.PORT || 7500;
 
